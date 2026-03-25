a2e1a2ef6dda8871381d29260f4f610fbf18bd3e
abhijth <Alpha.astute@gmail.com>
2026-03-25 08:24:25 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="overlay">
+        <h1>Paradise Nursery</h1>
+        <p>Welcome to our online plant shop</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
